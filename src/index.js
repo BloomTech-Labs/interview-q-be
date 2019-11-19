834e6610567a6a4b37d20f709720dc17bdbfc466
Jonathan Ho <johnnyho82@gmail.com>
2019-11-18 21:31:54 -0500

    Commit before pull

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const server = require("./server.js");
+const server = require('./server.js');
 
 const options = { PORT: process.env.PORT || 4000 };
 
 server.start(options, ({ PORT }) => {
-  console.log(`Running on ${PORT}`);
+	console.log(`Running on ${PORT}`);
 });
