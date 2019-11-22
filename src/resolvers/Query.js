3300e24182367d79d416d0db6f24de24e1dd32df
Jordan <jordandoan@hotmail.com>
2019-11-22 13:32:41 -0800

    Finished filter, should be good to go

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -17,15 +17,21 @@ function post(_parent, args, context){
 function posts(parent, args, context){
   // Create filter here
   let where =  {AND: []};
-  if (args.industry) {
-    where.AND.push({industry: {name: args.industry}});
-  }
+  where.AND.push({industry: {name: args.industry}});
   if (args.price) {
     let prices = args.price.split(',');
     where.AND.push({price_gte: Number(prices[0])});
     where.AND.push({price_lte: Number(prices[1])});
   }
-  return context.prisma.posts({where})
+  if (args.tags) {
+    let tags = args.tags.split(' ');
+    let idx = where.AND.push({tags_some: {OR: []}});
+    tags.forEach(tag => {
+      where.AND[idx-1].tags_some.OR.push({name: tag});
+    })
+  }
+
+  return context.prisma.posts({where, orderBy: args.orderBy})
 }
 
 function industry(_parent, args, context) {
