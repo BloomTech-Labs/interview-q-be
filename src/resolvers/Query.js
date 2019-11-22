210f20ad880496f566c2266329f495e4d84c013d
Jordan <jordandoan@hotmail.com>
2019-11-22 11:20:13 -0800

    got price and industry search working

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -14,8 +14,18 @@ function post(_parent, args, context){
   return context.prisma.post({ id: args.id })
 }
 
-function posts(_parent, _args, context){
-  return context.prisma.posts()
+function posts(parent, args, context){
+  // Create filter here
+  let where =  {AND: []};
+  if (args.industry) {
+    where.AND.push({industry: {name: args.industry}});
+  }
+  if (args.price) {
+    let prices = args.price.split(',');
+    where.AND.push({price_gte: Number(prices[0])});
+    where.AND.push({price_lte: Number(prices[1])});
+  }
+  return context.prisma.posts({where})
 }
 
 function industry(_parent, args, context) {
