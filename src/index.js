85dcf5a3e889ab4a2cabfd7bfaccb9e734e61ea4
Aaron Clark <aaronpclark92@gmail.com>
2020-02-01 12:14:15 -0600

    updated server running message

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const server = require('./server.js');
 const options = { port: process.env.APOLLO_PORT || 4000 };
 
 server.start(options, ({ port }) => {
-	console.log(`Running on ${port}`);
+	console.log(`QualityHub InterviewQ running on ${port}`);
 });
