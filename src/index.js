9904c218d315c1b953d3829e84a53f8fcd3a5212
Aaron Clark <aaronpclark92@gmail.com>
2020-02-04 12:09:36 -0600

    update port variable

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./server.js');
 
-const options = { port: process.env.APOLLO_PORT || 4000 };
+const options = { port: (process.env.APOLLO_PORT || process.env.PORT) || 4000 };
 
 server.start(options, ({ port }) => {
 	console.log(`QualityHub InterviewQ running on ${port}`);
